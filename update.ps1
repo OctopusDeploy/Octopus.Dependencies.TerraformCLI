5e3d79cbbe6756647adff851f31472755b120330
Ben Pearce <ben.pearce@octopus.com>
2018-12-03 09:10:02 +1000

    wip on script to download the plugins

--- update.ps1
+++ update.ps1
@@ -11,45 +11,69 @@ function Unzip
 # TLS 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-# download terraform.exe
-$version = Invoke-RestMethod -Uri "https://checkpoint-api.hashicorp.com/v1/check/terraform"
-Invoke-WebRequest -Uri "https://releases.hashicorp.com/terraform/$($version.current_version)/terraform_$($version.current_version)_windows_386.zip" -OutFile "terraform_$($version.current_version)_windows_386.zip"
-$cwd = (Get-Item -Path ".\" -Verbose).FullName
-Remove-Item terraform.exe
-Unzip "$cwd\terraform_$($version.current_version)_windows_386.zip" "$cwd"
+# read included plugins and versions
+$plugins = Get-Content("plugin-versions.json") | Out-String | ConvertFrom-Json
 
-# download latest version of supported plugins
-# this is a list of the names of the support plugins
-$plugins = @( "terraform-provider-azurerm", "terraform-provider-aws", "terraform-provider-azure");
+
+# download terraform.exe
+# $version = Invoke-RestMethod -Uri "https://checkpoint-api.hashicorp.com/v1/check/terraform"
+# Invoke-WebRequest -Uri "https://releases.hashicorp.com/terraform/$($version.current_version)/terraform_$($version.current_version)_windows_386.zip" -OutFile "terraform_$($version.current_version)_windows_386.zip"
+# $cwd = (Get-Item -Path ".\" -Verbose).FullName
+# Remove-Item terraform.exe
+# Unzip "$cwd\terraform_$($version.current_version)_windows_386.zip" "$cwd"
+
 
 $allVersions = [System.Net.WebClient]::new().DownloadString("https://releases.hashicorp.com/index.json") | ConvertFrom-Json
 
 foreach ($plugin in $allVersions.psobject.Properties ) {
-    $mostRecent = $null
-    if ($plugin.Name -in $plugins) {
-        foreach ($version in $plugin.Value.versions.psobject.Properties) {
-            if ($mostRecent -eq $null) {
-                $mostRecent = $version
-            } else {
-                ([int] $major, [int] $minor, [int] $patch) = $version.Value.version.Split('.') | % { iex $_ }
-                ([int] $currentMajor, [int] $currentMinor, [int] $currentPatch) = $mostRecent.Value.version.Split('.') | % { iex $_ }
-                if ($major -gt $currentMajor -or `
-                   ($major -eq $currentMajor -and $minor -gt $currentMinor) -or `
-                   ($major -eq $currentMajor -and $minor -eq $currentMinor -and $patch -gt $currentPatch)) {
-                    $mostRecent = $version
-                } 
-            }
-        }
+    $requestedPlugin = $plugins | Where-Object { $_.name -eq $plugin.Name}
+    #$requestedPlugin.name
+    if ($requestedPlugin) {
+        foreach ($requestedVersion in $requestedPlugin.versions) {
+            #$requestedVersion
+            #$plugin.Value.versions.psobject.Properties
+            $version = $plugin.Value.versions.psobject.Properties | Where-Object { $_.Value.version -eq $requestedVersion } | Select { $_.Value}
 
-        #write-host $plugin.Name $mostRecent.Value.version
-        foreach ($build in $mostRecent.Value.builds) {
-            if ($build.os -eq "windows" -and $build.arch -eq "386") {
-                $outFile = [System.IO.Path]::Combine($env:Temp, $build.filename)
-                write-host "Downloading" $build.filename 
-                Invoke-WebRequest -Uri $build.url -OutFile $outFile
-                $cwd = (Get-Item -Path ".\" -Verbose).FullName
-                Unzip $outFile "$cwd\plugins\windows_386"
+            if ($version) {
+                $version | % { $_.Value} 
+                foreach ($build in $version.Value.builds) {
+                    $build
+                    if ($build.os -eq "windows" -and $build.arch -eq "386") {
+                        $outFile = [System.IO.Path]::Combine($env:Temp, $version.filename)
+                        write-host "Downloading" $version.filename 
+                        Invoke-WebRequest -Uri $version.url -OutFile $outFile
+                        $cwd = (Get-Item -Path ".\" -Verbose).FullName
+                        Unzip $outFile "$cwd\plugins\windows_386"
+                    }
+                }
+            } else {
+                Write-Error "Could not find version $requestedVersion of ${$requestedPlugin.name}"
+                exit 1    
             }
         }
+        # foreach ($version in $plugin.Value.versions.psobject.Properties) {
+        #     if ($mostRecent -eq $null) {
+        #         $mostRecent = $version
+        #     } else {
+        #         ([int] $major, [int] $minor, [int] $patch) = $version.Value.version.Split('.') | % { iex $_ }
+        #         ([int] $currentMajor, [int] $currentMinor, [int] $currentPatch) = $mostRecent.Value.version.Split('.') | % { iex $_ }
+        #         if ($major -gt $currentMajor -or `
+        #            ($major -eq $currentMajor -and $minor -gt $currentMinor) -or `
+        #            ($major -eq $currentMajor -and $minor -eq $currentMinor -and $patch -gt $currentPatch)) {
+        #             $mostRecent = $version
+        #         } 
+        #     }
+        # }
+
+        # #write-host $plugin.Name $mostRecent.Value.version
+        # foreach ($build in $mostRecent.Value.builds) {
+        #     if ($build.os -eq "windows" -and $build.arch -eq "386") {
+        #         $outFile = [System.IO.Path]::Combine($env:Temp, $build.filename)
+        #         write-host "Downloading" $build.filename 
+        #         Invoke-WebRequest -Uri $build.url -OutFile $outFile
+        #         $cwd = (Get-Item -Path ".\" -Verbose).FullName
+        #         Unzip $outFile "$cwd\plugins\windows_386"
+        #     }
+        # }
     }
 }
